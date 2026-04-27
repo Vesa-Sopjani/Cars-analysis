5e1b96c8589e37fd4371b40f259a2213c65802df
ErinaTernava <et70055@ubt-uni.net>
2026-04-27 21:32:07 +0200

    created homepage

---
+++ frontend/src/app/components/price-suggester/price-suggester.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-price-suggester',
+  imports: [],
+  templateUrl: './price-suggester.html',
+  styleUrl: './price-suggester.scss',
+})
+export class PriceSuggester {}
